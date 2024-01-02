70805ff445f2bcf40b783b27c334f2df6f659e9d
GodOmParticle999 <134603570+GodOmParticle999@users.noreply.github.com>
2024-01-02 07:45:01 +0530

    added stack property to get more info of the error

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -1,7 +1,8 @@
 const errorHandler=(error,_,res,next)=>{
     res.status(error.status||500).json({
         status:error.status||500,
-        message:error.message||"something went wrong"
+        message:error.message||"something went wrong",
+        stack:error.stack
 })
 next(res)
 }
