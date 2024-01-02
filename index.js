018a46d2f19616ab58b0d2ff53257a3cb6f75190
GodOmParticle999 <134603570+GodOmParticle999@users.noreply.github.com>
2024-01-02 08:08:21 +0530

    added cookie parser middleware

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ import cors from "cors"
 import userRouter from "./routes/user.route.js"
 import dbConnect from "./db/db.js"
 import errorHandler from "./middlewares/errorHandler.js"
+import cookieParser from "cookie-parser"
 // import dotenv.config at the top
 dotenv.config()
 
@@ -20,11 +21,17 @@ dbConnect().then(()=>{
 }).catch((e)=>{
     console.log("db connection failed",e)
 })
+// middlewares
 
+// this middleware allows all the incoming request from client side 
 app.use(cors())
 
 app.use(Express.json())
+// this parses the cookie header and populates the req.cookies object in a key value paired manner 
+app.use(cookieParser)
 
+// this middleware leads to all the routes that come to user route
 app.use("/user",userRouter)
 
+// this middleware handles all kind of errors
 app.use(errorHandler)
