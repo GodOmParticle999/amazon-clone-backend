0c67fba57f6a6a45e6ac302d0267d876df70682e
GodOmParticle999 <134603570+GodOmParticle999@users.noreply.github.com>
2024-01-06 08:46:46 +0530

    added middlewares to identify user and admin

---
+++ middlewares/auth.js
@@ -0,0 +1,35 @@
+import jwt from "jsonwebtoken"
+import { ApiError } from "../utils/ApiError.js"
+import { User } from "../models/user.model.js"
+
+export const verifyJWT=async(req,res,next)=>{
+    // get token from cookies
+    const token=req.cookies.access_token
+    if(!token) return next(new ApiError(401,"unauthorized access!"))
+
+    const decodedToken=jwt.verify(token,process.env.JWT_SECRET_KEY)
+   try {
+    const user=await User.findById(decodedToken._id).select("-password ")
+    if(!user) return next(new ApiError(401,"invalid access token!"))
+    
+    // bind this user to req to be access further 
+    req.user=user
+    next()
+   } catch (error) {
+    return next(new ApiError(401,error?.message||"invalid access token!"))
+   }
+    
+  
+}
+//  to verify admin
+
+export const verifyAdmin=(req,_,next)=>{
+    verifyJWT(req,res,next,()=>{
+        // pass the isAdmin in payload while logging in
+        if(req.user.isAdmin){
+          next()
+        }else{
+            return next(new ApiError(403,"you're not authorized"))
+        }
+    })
+}
